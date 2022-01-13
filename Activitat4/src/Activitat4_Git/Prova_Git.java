8ee01a97ed694529aafa618fbe043d67dcf9589d
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2022-01-13 12:37:14 +0100

    No em funciona la sincronització, que sad :(

--- Activitat4/src/Activitat4_Git/Prova_Git.java
+++ Activitat4/src/Activitat4_Git/Prova_Git.java
@@ -8,5 +8,6 @@ public class Prova_Git {
 	public static void Prova_Git() {
 		System.out.println("Aquesta es la meva primera prova");
 		System.out.println("This isn't working, don't try");
+		System.out.println("I am really sad actually :(");
 	}
 }

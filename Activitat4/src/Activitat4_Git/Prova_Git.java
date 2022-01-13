5382e0e52510ed33eb266d771781019d6d843501
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2022-01-13 11:42:39 +0100

    Primer commit fent servir eGit

---
+++ Activitat4/src/Activitat4_Git/Prova_Git.java
@@ -0,0 +1,11 @@
+package Activitat4_Git;
+
+public class Prova_Git {
+
+	public static void main(String[] args) {
+		Prova_Git();
+	}
+	public static void Prova_Git() {
+		System.out.println("Aquesta es la meva primera prova");
+	}
+}
---
+++ Activitat4/src/Activitat4_Git/Prova_Git2.java
@@ -0,0 +1,12 @@
+package Activitat4_Git;
+
+public class Prova_Git2 {
+
+	public static void main(String[] args) {
+		Prova_Git2();
+
+	}
+	public static void Prova_Git2() {
+		System.out.println("Aquesta es la meva segona prova");
+	}
+}

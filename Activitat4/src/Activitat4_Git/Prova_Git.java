adf458ef2e4616fcfb0c1537be3ea84c69353e7f
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2022-01-13 12:30:43 +0100

    Segonda modificació :(

--- Activitat4/src/Activitat4_Git/Prova_Git.java
+++ Activitat4/src/Activitat4_Git/Prova_Git.java
@@ -7,5 +7,6 @@ public class Prova_Git {
 	}
 	public static void Prova_Git() {
 		System.out.println("Aquesta es la meva primera prova");
+		System.out.println("This isn't working, don't try");
 	}
 }
